93cddcd20a8abe76e97a56315fcd9572be2e7e54
Ozlemkaragoz4 <36725386+Ozlemkaragoz4@users.noreply.github.com>
2019-11-27 17:33:39 +0300

    Create 2019_09_11_074227_create_product_table.php

---
+++ Kozmo/database/migrations/2019_09_11_074227_create_product_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProductTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('product', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('title');
+            $table->integer('category_id');
+            $table->string('content');
+            $table->string('image');
+            $table->integer('price');
+            $table->integer('discount');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('product');
+    }
+}
