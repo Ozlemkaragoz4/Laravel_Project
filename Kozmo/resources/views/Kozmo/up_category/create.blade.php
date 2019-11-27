422f608eb11b78795e4223354fd89f8f8979240f
Ozlemkaragoz4 <36725386+Ozlemkaragoz4@users.noreply.github.com>
2019-11-27 17:54:00 +0300

    Create create.blade.php

---
+++ Kozmo/resources/views/Kozmo/up_category/create.blade.php
@@ -0,0 +1,34 @@
+@extends('Kozmo.main')
+@section('content')
+    <div class="">
+        <div class="page-title">
+            <div class="title_left">
+                <h3>Üst Kategori <small>Ekle</small></h3>
+            </div>
+        </div>
+
+        <div class="clearfix"></div>
+
+        <div class="row">
+            <div class="col-md-12">
+                <div class="x_panel">
+                    <form class="form-horizontal form-label-left" action="{{route('Kozmo.up_category.create')}}" method="post" enctype="multipart/form-data">
+                        @csrf
+                        @method('POST')
+                        <div class="form-group">
+                            <h2>Üst Kategori Başlık</h2>
+                            <div class="col-sm-12">
+                                <input type="text" class="form-control" id="title" name="title" placeholder="Yeni Başlık">
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <div class="col-sm-12">
+                                <button type="submit" class="btn btn-success">Kaydet</button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection()
