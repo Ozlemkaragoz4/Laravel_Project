1270a7e30dcb34477b9680dda0ec44939fe5a8b2
Ozlemkaragoz4 <36725386+Ozlemkaragoz4@users.noreply.github.com>
2019-11-27 17:29:49 +0300

    Create Order.php

---
+++ Kozmo/app/Model/Order.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Model;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Order extends Model
+{
+    public $table="order";
+    protected $fillable=[
+        'user_id'
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class,'user_id');
+    }
+}
